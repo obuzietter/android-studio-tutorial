dbbcfff3fc43bcc7c278c5e89d1f2eeb764102c2
obuzietter <obuyacalvince672@gmail.com>
2023-07-24 18:26:44 +0300

    passing message between 2 activities using explicit intent

--- app/src/main/java/com/example/myapplication/SecondActivity.kt
+++ app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -4,6 +4,7 @@ package com.example.myapplication
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class SecondActivity : AppCompatActivity() {
@@ -12,7 +13,10 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
-
+        val bundle : Bundle? = intent.extras
+        val message = bundle!!.getString("m")
+        val textView : TextView = findViewById(R.id.messageTV)
+        textView.text = message
 
 
 
